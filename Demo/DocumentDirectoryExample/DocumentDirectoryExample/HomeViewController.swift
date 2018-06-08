b2545f289fed8f74d591cded90142736b2e01b80
MBA0202 <tanh.le@asiantech.vn>
2018-06-08 20:34:23 +0700

    demo

---
+++ Demo/DocumentDirectoryExample/DocumentDirectoryExample/HomeViewController.swift
@@ -0,0 +1,105 @@
+//
+//  HomeViewController.swift
+//  DocumentDirectoryExample
+//
+//  Created by MBA0202 on 5/27/18.
+//  Copyright © 2018 MBA0202. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+var i = 0
+var arrImageName: [String] = []
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    var imagePickerController: UIImagePickerController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    func saveImage(imageName: String) {
+        let fileManager = FileManager.default
+        let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imageName)
+        let image = photoImageView.image!
+        let data = UIImagePNGRepresentation(image)
+        fileManager.createFile(atPath: imagePath as String, contents: data, attributes: nil)
+        if fileManager.fileExists(atPath: imagePath) {
+            alert(title: "Message", msg: "Photo saved successfully", buttons: ["OK"]) { (actions) in
+                guard let title = actions.title else { return }
+                switch title {
+                default: break
+                }
+            }
+        } else {
+            alert(title: "Message", msg: "Photo cannot save", buttons: ["OK"]) { (actions) in
+                guard let title = actions.title else { return }
+                switch title {
+                default: break
+                }
+            }
+        }
+    }
+
+    @IBAction func gotoCameraTouchUpInside(_ sender: UIButton) {
+        imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .camera
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
+    @IBAction func gotoAlbumTouchUpInside(_ sender: UIButton) {
+        navigationController?.pushViewController(AlbumViewController(), animated: true)
+    }
+    
+    @IBAction func savePhotoButtonTouchInside(_ sender: UIButton) {
+        i += 1
+        let name = "test\(i).png"
+        arrImageName.append(name)
+        saveImage(imageName: name)
+    }
+    
+    @IBAction func gotoCameraRoll(_ sender: UIButton) {
+        let status = PHPhotoLibrary.authorizationStatus()
+        switch (status) {
+        case PHAuthorizationStatus.notDetermined:
+            PHPhotoLibrary.requestAuthorization { (status) -> Void in
+                if status == PHAuthorizationStatus.authorized {
+                    self.present(ImagePickerViewController(), animated: true, completion: nil)
+                }
+            }
+        case PHAuthorizationStatus.authorized:
+            present(ImagePickerViewController(), animated: true, completion: nil)
+        case PHAuthorizationStatus.restricted, PHAuthorizationStatus.denied:
+            break
+        }
+    }
+}
+
+extension HomeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        imagePickerController.dismiss(animated: true, completion: nil)
+        photoImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+}
+
+extension UIViewController {
+    
+    func alert(error: Error) {
+        alert(title: "ERROR", msg: error.localizedDescription, buttons: ["OK"], handler: nil)
+    }
+    
+    func alert(title: String? = nil, msg: String, buttons: [String], handler: ((UIAlertAction) -> Void)?) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        for button in buttons {
+            let action = UIAlertAction(title: button, style: .default, handler: { action in
+                handler?(action)
+            })
+            alert.addAction(action)
+        }
+        present(alert, animated: true, completion: nil)
+    }
+}
---
+++ Demo/VideoBackgorund/VideoBackgorund/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  VideoBackgorund
+//
+//  Created by MBA0202 on 6/5/18.
+//  Copyright © 2018 MBA0202. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
