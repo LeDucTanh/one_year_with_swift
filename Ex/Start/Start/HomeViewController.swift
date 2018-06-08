f84b835f37ce5257fb5c2756ea9af2b1090747d3
Anh Tuấn <atuanbk94@gmail.com>
2018-06-08 23:18:03 +0700

    no message

--- Ex/Start/Start/HomeViewController.swift
+++ Ex/Start/Start/HomeViewController.swift
@@ -12,7 +12,26 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let frame = CGRect(x: 50, y: 100, width: 200, height: 300)
+        let userAvatar = UIImageView(image: UIImage(named: "123.jpg"))
+        userAvatar.frame = frame
+        userAvatar.contentMode = .scaleToFill
+        view.addSubview(userAvatar)
+        // add user name
+        let userName = UILabel(frame: CGRect(x: 50, y: 450, width: 200, height: 50))
+        userName.text = "User name"
+        userName.backgroundColor = .lightGray
+        userName.textColor = .blue
+        view.addSubview(userName)
+        
+        //add button
+        let button = UIButton(frame: CGRect(x: 50, y: 100, width: 100, height: 250))
+        button.backgroundColor = .clear
+        button.addTarget(self , action: #selector(buttonDidClick), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    @objc func buttonDidClick() {
+        print("Button is clicked!")
         // Do any additional setup after loading the view.
     }
 
